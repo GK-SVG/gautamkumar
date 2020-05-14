9bdca001d6e5f43f62742d06e2c4f5328e613313
gk-svg <gk32239@gmail.com>
2020-05-15 01:19:35 +0530

    models for projects and skills

--- home/models.py
+++ home/models.py
@@ -15,4 +15,16 @@ class Plateform(models.Model):
     org_image=models.CharField(max_length=265)
     def __str__(self):
         return self.org_name
+
+class Skills(models.Model):
+    sk_name=models.CharField(max_length=100)
+    sk_type=models.CharField(max_length=50)
+    def __str__(self):
+        return self.sk_name
+    
+class MyProjects(models.Model):
+    project_name=models.CharField(max_length=100)
+    project_link=models.CharField(max_length=100)
+    def __str__(self):
+        return self.project_name
     

d6a3650917ae75de03a956482deb410d2dea3721
gk-svg <gk32239@gmail.com>
2020-05-15 01:21:36 +0530

    making site atractive

--- home/admin.py
+++ home/admin.py
@@ -3,3 +3,5 @@ from home.models import *
 # Register your models here.
 admin.site.register(Certificate)
 admin.site.register(Plateform)
+admin.site.register(MyProjects)
+admin.site.register(Skills)

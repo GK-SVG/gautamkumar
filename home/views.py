fbba0996054bccc5e2096c692bbaa99be07d38a1
gk-svg <gk32239@gmail.com>
2020-05-15 09:44:50 +0530

    home and about view merge

--- home/views.py
+++ home/views.py
@@ -1,17 +1,15 @@
 from django.shortcuts import render,HttpResponse
 from .models import Plateform,Certificate,Skills,MyProjects
 
-def index(request):
-    return render(request,'index.html')
-
 def plateform(request):
     plateforms=Plateform.objects.all()
     return render(request,'plateform.html',{'plateforms':plateforms})
 
 
-def about(request):
+def index(request):
+    projects=MyProjects.objects.all()
     skills=Skills.objects.all()
-    return render(request,'about.html',{'skills':skills})
+    return render(request,'index.html',{'skills':skills,'projects':projects})
 
 def services(request):
     certificates=Certificate.objects.all()

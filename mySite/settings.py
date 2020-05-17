30b809f38a21f48e8a1864de57dee61d5708efaa
gk-svg <gk32239@gmail.com>
2020-05-17 22:56:41 +0530

    staticfiles path added for deploment

--- mySite/settings.py
+++ mySite/settings.py
@@ -118,7 +118,7 @@ USE_L10N = True
 
 USE_TZ = True
 
-#STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
